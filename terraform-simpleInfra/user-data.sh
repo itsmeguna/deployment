315d6fd9fca89246e6647614f371f35cc5f56e91
itsmeguna <itsmegunam@gmail.com>
2023-12-12 13:29:43 +0530

    terraform files 1 vpc and 1EC2 instance

---
+++ terraform-simpleInfra/user-data.sh
@@ -0,0 +1,70 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+/bin/echo "Hello World" >> /tmp/testfile.txt
+# Update the system
+sudo apt update -y
+
+# Install Docker dependencies
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add the Docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Add the Docker repository
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the system again
+sudo apt update -y
+
+# Install Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Add the current user to the Docker group
+sudo usermod -aG docker $USER
+
+# Add Permission for docker.sock
+sudo chmod 666 /var/run/docker.sock
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Install unzip
+sudo apt install -y unzip
+
+# Install the AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Install AWS Systems Manager Session Manager
+sudo snap install amazon-ssm-agent --classic
+
+# Reload the Docker daemon
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+# Verify the installation
+aws --version
+docker --version
+docker-compose --version
+session-manager-plugin --version
+--//--
